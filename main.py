92535a34ca9d0b9517db16127874b41a4cacefd6
Andrey Alekseev <leksus@leksus.info>
2023-11-13 17:09:22 +0300

    add logging with notification in tg

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ from environs import Env
 import logging
 from playwright.sync_api import sync_playwright
 import telegram
+import time
 
 
-def play(page, nie, name):
+DELAY_BETWEEN_REQUESTS = 600  # 10 minutes
+MAX_COUNT_EXCEPTIONS = 3
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        self.tg_bot.send_message(
+            chat_id=self.chat_id,
+            text=self.format(record),
+        )
+
+
+def is_cita_available(page, nie, name):
 
     page.get_by_role('combobox', name='Selecciona Oficina:').select_option('14')
     page.get_by_role('combobox', name='TRÁMITES POLICÍA NACIONAL').select_option('4036')
@@ -32,6 +53,22 @@ def main():
     chat_id = env('TG_CHAT_ID')
     bot = telegram.Bot(token=tg_token)
 
+
+    formatter = logging.Formatter('%(asctime)s %(message)s', '%Y-%m-%d %H:%M:%S')
+
+    tg_log_handler = TelegramLogsHandler(bot, chat_id)
+    tg_log_handler.setFormatter(formatter)
+    tg_log_handler.setLevel(logging.INFO)
+
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setFormatter(formatter)
+    stdout_handler.setLevel(logging.DEBUG)
+
+    logger.addHandler(tg_log_handler)
+    logger.addHandler(stdout_handler)
+
+    logger.debug('Бот стартовал')
+
     check_url = 'https://icp.administracionelectronica.gob.es/icpplustieb/citar?p=8&locale=es'
 
     with sync_playwright() as playwright:
@@ -41,19 +78,23 @@ def main():
         )
         context = browser.new_context()
         page = context.new_page()
-        page.goto(check_url)
-        try:
-            has_cita = play(page, nie, name)
-            if has_cita:
-                bot.send_message(
-                    chat_id=chat_id,
-                    text=f'Походу сита есть, бегом [записываться]({check_url})\!',
-                    parse_mode='MarkdownV2',
-                )
-            else:
-                print('Ситы нема')
-        except Exception as e:
-            print(e)
+
+        count_exceptions = 0
+        while True:
+            if count_exceptions >= MAX_COUNT_EXCEPTIONS:
+                break
+            try:
+                page.goto(check_url)
+                if is_cita_available(page, nie, name):
+                    logger.info(f'Походу сита есть, бегом записываться')
+                    timestamp = time.strftime('%Y/%m/%d_%H_%M_%S', time.localtime())
+                    page.screenshot(path=f'screenshot_{timestamp}.png')
+                else:
+                    logging.debug('Сит нема :(')
+                time.sleep(DELAY_BETWEEN_REQUESTS)
+            except Exception as e:
+                logger.exception(e)
+                count_exceptions += 1
 
 # The requested URL was rejected. Please consult with your administrador.
 
